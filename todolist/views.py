ff24cd0c2e599ac8a9df3a8016999d644d0bcf24
diwap <dipipandey1@gmail.com>
2019-03-10 17:21:26 +0545

    filter with tag added

--- todolist/views.py
+++ todolist/views.py
@@ -7,6 +7,15 @@ class TodoListViewset(viewsets.ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
 
+    def get_queryset(self):
+        queryset = TodoList.objects.all()
+        tag = self.request.query_params.get('tag', None)
+        if tag is not None:
+            queryset = TodoList.objects.filter(tags__slug=tag)
+        return queryset
+
+
+
 class TagsViewset(viewsets.ModelViewSet):
     queryset = Tags.objects.all()
     serializer_class = TagSerializer
